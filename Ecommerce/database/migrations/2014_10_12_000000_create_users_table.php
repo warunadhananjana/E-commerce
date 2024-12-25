dce11c00258ae71cbd4043fc3807bba5a319191a
dhananjana123 <warunad999@gmail.com>
2024-12-25 07:45:31 +0530

    add register

--- Ecommerce/database/migrations/2014_10_12_000000_create_users_table.php
+++ Ecommerce/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('usertype')->default(0); // Use integer if it's a role ID
+            $table->string('phone')->nullable(); // Use lowercase for consistency
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
